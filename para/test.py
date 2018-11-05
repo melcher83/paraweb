70a6b7f16bf27c4c88a464fcfc326b7d3abe7cee
jmelcher <jonathan@bluewiretech.com>
2018-11-05 16:17:52 -0600

    fixed typo

--- para/test.py
+++ para/test.py
@@ -13,6 +13,8 @@ natlas_obj.snmp_add_credential(snmp_ver, snmp_community)
 print(config)
 natlas_obj.config_load(config)
 
+natlas_obj.set_discover_maxdepth(10)
 
 
-natlas_obj.discover_network("10.200.8.1", 1)
+
+natlas_obj.discover_network("192.168.254.5", 1)
