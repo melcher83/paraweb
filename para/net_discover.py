9c937fa9d51cca2950b9eaac1491e44e291ebe0f
jmelcher <jonathan@bluewiretech.com>
2018-11-06 12:32:54 -0600

    Started making functions for network discovery

--- para/net_discover.py
+++ para/net_discover.py
@@ -1,19 +1,20 @@
 import natlas.natlas.natlas
 from para.models import Client
-import _thread
+
 
 class discover:
     def __init__(self, pk):
-        form1 = Client.objects.get(id=pk)
-        natlas_obj = natlas.natlas.natlas()
+        form1 = Client.objects.get(id=pk) #get client onject
+        natlas_obj = natlas.natlas.natlas() #create natlas object
 
         config = "web_app/templates/natlas.conf"
-        form1 = Client.objects.get(id=pk)
 
-        natlas_obj.config_load(config)
+        natlas_obj.config_load(config) #load config
 
-        natlas_obj.set_discover_maxdepth(10)
+        natlas_obj.set_discover_maxdepth(100) # set discovery depth
 
-        natlas_obj.snmp_add_credential(2, form1.snmp_com)
+        natlas_obj.snmp_add_credential(2, form1.snmp_com) #add snmp credentials
 
-        natlas_obj.discover_network(form1.rootip, 1))
+        natlas_obj.discover_network(form1.rootip, 1)) #discover network
+        nodes=natlas_obj.get_discovered_nodes()
+        print(nodes)
